a711e5d9749e4c938b8bedac0eaaaadb93ee2fb8
jansenv <jansenv44@gmail.com>
2020-01-27 10:59:36 -0600

    selective rendering

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { LocationProvider } from "./locations/LocationProvider"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import LocationList from "./locations/LocationList"
+import AnimalList from "./animal/AnimalList"
+import { CustomerProvider } from "./customers/CustomerProvider"
+import CustomerList from "./customers/CustomerList"
+import { EmployeeProvider } from "./employees/EmployeeProvider"
+import EmployeeList from "./employees/EmployeeList"
+
+export default (props) => {
+    return (
+        <>
+            <LocationProvider>
+                {/* Render the location list when http://localhost:3000/ */}
+                <Route exact path="/">
+                    <LocationList />
+                </Route>
+            </LocationProvider>
+
+            <AnimalProvider>
+                {/* Render the animal list when http://localhost:3000/ */}
+                <Route path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
+
+            <CustomerProvider>
+                <Route path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
+
+            <EmployeeProvider>
+                <Route path="/employees">
+                    <EmployeeList />
+                </Route>
+            </EmployeeProvider>
+        </>
+    )
+}
--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,54 +1,67 @@
 import React from "react"
+import { Route } from "react-router-dom"
 import "./Kennel.css"
-import "./animal/Animals.css"
-import "./customers/Customer.css"
-import "./employees/Employee.css"
-import "./locations/Location.css"
-import { LocationProvider } from "./locations/LocationProvider"
-import LocationList from "./locations/LocationList"
-import { AnimalProvider } from "./animal/AnimalProvider"
-import AnimalList from "./animal/AnimalList"
-import { CustomerProvider } from "./customers/CustomerProvider"
-import CustomerList from "./customers/CustomerList"
-import { EmployeeProvider } from "./employees/EmployeeProvider"
-import EmployeeList from "./employees/EmployeeList"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
 
 export default () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalProvider>
-                <AnimalList />
-            </AnimalProvider>
-        </article>
-
-        <h2>Employees</h2>
-        <article className="employees">
-            <EmployeeProvider>
-                <EmployeeList />
-            </EmployeeProvider>
-        </article>
-
-        <h2>Locations</h2>
-        <article className="locations">
-            <LocationProvider>
-                <LocationList />
-            </LocationProvider>
-        </article>
-
-        <h2>Customers</h2>
-        <article className="customers">
-            <CustomerProvider>
-                <CustomerList />
-            </CustomerProvider>
-        </article>
+    <NavBar />
+    <ApplicationViews />
     </>
 )
+
+// import React from "react"
+// import "./Kennel.css"
+// import "./animal/Animals.css"
+// import "./customers/Customer.css"
+// import "./employees/Employee.css"
+// import "./locations/Location.css"
+// import { LocationProvider } from "./locations/LocationProvider"
+// import LocationList from "./locations/LocationList"
+// import { AnimalProvider } from "./animal/AnimalProvider"
+// import AnimalList from "./animal/AnimalList"
+// import { CustomerProvider } from "./customers/CustomerProvider"
+// import CustomerList from "./customers/CustomerList"
+// import { EmployeeProvider } from "./employees/EmployeeProvider"
+// import EmployeeList from "./employees/EmployeeList"
+
+// export default () => (
+//     <>
+//         <h2>Nashville Kennels</h2>
+//         <small>Loving care when you're not there.</small>
+
+//         <address>
+//             <div>Visit Us at the Nashville North Location</div>
+//             <div>500 Puppy Way</div>
+//         </address>
+
+//         <h2>Animals</h2>
+//         <article className="animals">
+//             <AnimalProvider>
+//                 <AnimalList />
+//             </AnimalProvider>
+//         </article>
+
+//         <h2>Employees</h2>
+//         <article className="employees">
+//             <EmployeeProvider>
+//                 <EmployeeList />
+//             </EmployeeProvider>
+//         </article>
+
+//         <h2>Locations</h2>
+//         <article className="locations">
+//             <LocationProvider>
+//                 <LocationList />
+//             </LocationProvider>
+//         </article>
+
+//         <h2>Customers</h2>
+//         <article className="customers">
+//             <CustomerProvider>
+//                 <CustomerList />
+//             </CustomerProvider>
+//         </article>
+//     </>
+// )
