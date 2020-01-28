364a8f7f2ab982578edd43cc8302a16266f8bb90
Jansen <jansenv44@gmail.com>
2020-01-28 07:09:47 -0600

    added login/register forms

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,67 +1,27 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
 import "./Kennel.css"
 import NavBar from "./nav/NavBar"
 import ApplicationViews from "./ApplicationViews"
+import Login from "./auth/Login"
+import Register from "./auth/Register"
 
 export default () => (
     <>
-    <NavBar />
-    <ApplicationViews />
+        <Route render = {() => {
+            if (localStorage.getItem("kennel_customer")) {
+                return (
+                    <>
+                        <Route render = {props => <NavBar {...props} />} />
+                        <Route render = {props => <ApplicationViews {...props} />} />
+                    </>
+                )
+            } else {
+                return <Redirect to = "/login" />
+            }
+        }} />
+
+        <Route path = "/login" render = {props => <Login {...props} />} />
+        <Route path = "/register" render = {props => <Register {...props} />} />
     </>
 )
-
-// import React from "react"
-// import "./Kennel.css"
-// import "./animal/Animals.css"
-// import "./customers/Customer.css"
-// import "./employees/Employee.css"
-// import "./locations/Location.css"
-// import { LocationProvider } from "./locations/LocationProvider"
-// import LocationList from "./locations/LocationList"
-// import { AnimalProvider } from "./animal/AnimalProvider"
-// import AnimalList from "./animal/AnimalList"
-// import { CustomerProvider } from "./customers/CustomerProvider"
-// import CustomerList from "./customers/CustomerList"
-// import { EmployeeProvider } from "./employees/EmployeeProvider"
-// import EmployeeList from "./employees/EmployeeList"
-
-// export default () => (
-//     <>
-//         <h2>Nashville Kennels</h2>
-//         <small>Loving care when you're not there.</small>
-
-//         <address>
-//             <div>Visit Us at the Nashville North Location</div>
-//             <div>500 Puppy Way</div>
-//         </address>
-
-//         <h2>Animals</h2>
-//         <article className="animals">
-//             <AnimalProvider>
-//                 <AnimalList />
-//             </AnimalProvider>
-//         </article>
-
-//         <h2>Employees</h2>
-//         <article className="employees">
-//             <EmployeeProvider>
-//                 <EmployeeList />
-//             </EmployeeProvider>
-//         </article>
-
-//         <h2>Locations</h2>
-//         <article className="locations">
-//             <LocationProvider>
-//                 <LocationList />
-//             </LocationProvider>
-//         </article>
-
-//         <h2>Customers</h2>
-//         <article className="customers">
-//             <CustomerProvider>
-//                 <CustomerList />
-//             </CustomerProvider>
-//         </article>
-//     </>
-// )
